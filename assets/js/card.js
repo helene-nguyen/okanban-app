e188cbb8fadd71d150071a462a5ae166739b405e
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-06-25 07:06:24 +0200

    [feat] add delete card by list id

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,7 +1,7 @@
 //~import modules
-import { dragCard } from "./drag_drop/card.js";
-import { listModule, tagModule, url, allCards, converter } from "./index.js";
-import { displayNotification } from "./utils.js";
+import { dragCard } from './drag_drop/card.js';
+import { listModule, tagModule, url, allCards, allLists, converter } from './index.js';
+import { displayNotification } from './utils.js';
 
 const cardModule = {
   //*FETCH ALL CARDS BY LIST ID
@@ -25,22 +25,22 @@ const cardModule = {
      */
   //*SHOW CARD MODAL
   showAddCardModal(event) {
-    const listId = event.target.closest(".my-list").dataset.listId;
+    const listId = event.target.closest('.my-list').dataset.listId;
     //add data attribute
-    document.querySelector(".card-list-id").value = listId;
+    document.querySelector('.card-list-id').value = listId;
     //reset info in inputs
-    document.querySelector(`#addCardModal input[name='card_title']`).value = "";
-    document.querySelector(`#addCardModal input[name='card_description']`).value = "";
+    document.querySelector(`#addCardModal input[name='card_title']`).value = '';
+    document.querySelector(`#addCardModal input[name='card_description']`).value = '';
 
-    const modalElement = document.getElementById("addCardModal");
-    modalElement.classList.add("is-active");
-    modalElement.querySelector(".card-list-id").value = listId;
+    const modalElement = document.getElementById('addCardModal');
+    modalElement.classList.add('is-active');
+    modalElement.querySelector('.card-list-id').value = listId;
   },
 
   //*HIDE CARD MODAL
   hideModalCard() {
-    const modalElement = document.getElementById("addCardModal");
-    modalElement.classList.remove("is-active");
+    const modalElement = document.getElementById('addCardModal');
+    modalElement.classList.remove('is-active');
   },
 
   /**
@@ -53,16 +53,16 @@ const cardModule = {
     //get datas from card form
     const data = new FormData(event.target);
 
-    const cardTitle = data.get("card_title");
-    const cardDescription = data.get("card_description");
-    const cardColor = data.get("card_color");
-    const cardUser = data.get("card_user");
-    const listId = data.get("list_id");
+    const cardTitle = data.get('card_title');
+    const cardDescription = data.get('card_description');
+    const cardColor = data.get('card_color');
+    const cardUser = data.get('card_user');
+    const listId = data.get('list_id');
 
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         title: cardTitle,
@@ -81,7 +81,7 @@ const cardModule = {
       location.reload();
     }
 
-    throw new Error("Cannot create this card, server error");
+    throw new Error('Cannot create this card, server error');
   },
   /**
    * 
@@ -96,23 +96,23 @@ const cardModule = {
   //*MAKE NEW CARD
   makeCardInDOM(cardId, cardInfo, cardDescription, color, listId, cardOrder, cardUser) {
     //~Cloning template
-    const template = document.querySelector("#template-card");
+    const template = document.querySelector('#template-card');
     const clone = document.importNode(template.content, true);
-    const cloneBlockElement = clone.querySelector(".card-block-to-clone");
-    const card = clone.querySelector(".my-card");
+    const cloneBlockElement = clone.querySelector('.card-block-to-clone');
+    const card = clone.querySelector('.my-card');
     //~set data cards id
-    card.setAttribute("data-card-id", `${cardId}`);
+    card.setAttribute('data-card-id', `${cardId}`);
     //~set info card
-    card.querySelector(".card-info").textContent = cardInfo;
-    card.querySelector(".card-description").textContent = cardDescription;
-    card.querySelector(".card-order").value = cardOrder;
-    card.querySelector(".card-user").value = cardUser;
-    card.querySelector(".card-list-id").value = listId;
+    card.querySelector('.card-info').textContent = cardInfo;
+    card.querySelector('.card-description').textContent = cardDescription;
+    card.querySelector('.card-order').value = cardOrder;
+    card.querySelector('.card-user').value = cardUser;
+    card.querySelector('.card-list-id').value = listId;
     card.style.borderTop = `4px solid`;
     //color in HEX
     card.style.borderTopColor = color;
 
-    document.querySelector(`[data-list-id='${listId}']`).querySelector(".panel-block").insertAdjacentElement("afterbegin", cloneBlockElement);
+    document.querySelector(`[data-list-id='${listId}']`).querySelector('.panel-block').insertAdjacentElement('afterbegin', cloneBlockElement);
 
     cardModule.hideModalCard();
     cardModule.buttonEditCard();
@@ -121,7 +121,7 @@ const cardModule = {
     const cardsElement = document.querySelectorAll(`[data-card-id='${cardId}']`);
 
     for (const card of cardsElement) {
-      card.querySelector("#container-new-tag").addEventListener("submit", event => {
+      card.querySelector('#container-new-tag').addEventListener('submit', event => {
         event.preventDefault();
         //for each card, we want to add tags
         tagModule.handleAddTagsForm(cardId, event);
@@ -134,23 +134,23 @@ const cardModule = {
 
   //*BUTTON REMOVE CARD
   buttonRemoveCard() {
-    const buttonsRemoveCard = document.querySelectorAll(".remove-card");
+    const buttonsRemoveCard = document.querySelectorAll('.remove-card');
 
     for (const buttonRemove of buttonsRemoveCard) {
-      buttonRemove.addEventListener("click", cardModule.confirmModalDeleteCard);
+      buttonRemove.addEventListener('click', cardModule.confirmModalDeleteCard);
     }
   },
 
   //*BUTTON SHOW MODAL REMOVE CARD
   confirmModalDeleteCard(event) {
-    const cardToRemove = event.target.closest(".my-card");
+    const cardToRemove = event.target.closest('.my-card');
     const cardId = cardToRemove.dataset.cardId;
 
     //add pop up window to confirm deletion
-    const confirmDeleteBtnElement = document.querySelector("#removeModal");
-    confirmDeleteBtnElement.classList.add("is-active");
-    confirmDeleteBtnElement.querySelector(".card-id").value = cardId;
-    confirmDeleteBtnElement.querySelector(".form-delete").addEventListener("submit", cardModule.buttonConfirmDeleteCard);
+    const confirmDeleteBtnElement = document.querySelector('#removeModal');
+    confirmDeleteBtnElement.classList.add('is-active');
+    confirmDeleteBtnElement.querySelector('.card-id').value = cardId;
+    confirmDeleteBtnElement.querySelector('.form-delete').addEventListener('submit', cardModule.buttonConfirmDeleteCard);
 
     listModule.closeModalDeleteBtn();
   },
@@ -159,12 +159,12 @@ const cardModule = {
   async buttonConfirmDeleteCard(event) {
     event.preventDefault();
 
-    const cardIdToRemove = document.querySelector("#removeModal").querySelector(".card-id").value;
+    const cardIdToRemove = document.querySelector('#removeModal').querySelector('.card-id').value;
 
     const cardToRemove = document.querySelector(`[data-card-id='${cardIdToRemove}']`);
 
     const options = {
-      method: "DELETE"
+      method: 'DELETE'
     };
 
     const response = await fetch(`${url}${allCards}/${cardIdToRemove}`, options);
@@ -176,36 +176,51 @@ const cardModule = {
       //trick to see deletion immediately
       cardToRemove.remove();
     } else {
-      throw new Error("Cannot delete this card, server error");
+      throw new Error('Cannot delete this card, server error');
+    }
+  },
+
+  //*DELETE CARDS BY LIST ID
+  async doRemoveCardByListId(listId) {
+    const options = {
+      method: 'DELETE'
+    };
+
+    const response = await fetch(`${url}${allLists}/${listId}${allCards}`, options);
+
+    if (response.ok) {
+      const message = await response.json();
+    } else {
+      throw new Error('Cannot delete this card, server error');
     }
   },
 
   //*BUTTON EDIT CARD
   buttonEditCard() {
-    const buttonsEditCard = document.querySelectorAll(".edit-card");
+    const buttonsEditCard = document.querySelectorAll('.edit-card');
 
     for (const buttonEdit of buttonsEditCard) {
-      buttonEdit.addEventListener("click", cardModule.editCard);
+      buttonEdit.addEventListener('click', cardModule.editCard);
     }
   },
 
   //*DO EDIT CARD
   editCard(event) {
-    const cardElement = event.target.closest(".my-card");
+    const cardElement = event.target.closest('.my-card');
     const cardId = cardElement.dataset.cardId;
-    const listId = cardElement.querySelector(".card-list-id").value;
-    const cardOrder = cardElement.querySelector(".card-order").value;
-    const cardUser = cardElement.querySelector(".card-user").value;
+    const listId = cardElement.querySelector('.card-list-id').value;
+    const cardOrder = cardElement.querySelector('.card-order').value;
+    const cardUser = cardElement.querySelector('.card-user').value;
     //~color
     let currentColorCard = cardElement.style.borderTopColor;
     //convert RGB color in Hex color
-    let valueRGB = currentColorCard.split("(")[1].split(")")[0];
+    let valueRGB = currentColorCard.split('(')[1].split(')')[0];
     currentColorCard = converter.getHexFromRGB(valueRGB);
 
     cardModule.showEditCardModal(cardId, listId, cardOrder, cardUser, currentColorCard);
 
     //~valid form card
-    document.querySelector("#form-edit-card").addEventListener("submit", cardModule.handleEditCardForm);
+    document.querySelector('#form-edit-card').addEventListener('submit', cardModule.handleEditCardForm);
   },
   /**
    * 
@@ -217,56 +232,56 @@ const cardModule = {
    */
   //*SHOW EDIT CARD MODAL
   showEditCardModal(cardId, listId, cardOrder, cardUser, currentColor) {
-    document.querySelector(`#editCardModal input[name='card_edit']`).value = "";
-    document.querySelector(`#editCardModal input[name='card_description']`).value = "";
+    document.querySelector(`#editCardModal input[name='card_edit']`).value = '';
+    document.querySelector(`#editCardModal input[name='card_description']`).value = '';
 
-    const editModalElement = document.getElementById("editCardModal");
+    const editModalElement = document.getElementById('editCardModal');
 
-    editModalElement.classList.add("is-active");
-    editModalElement.querySelector(".card-id").value = cardId;
-    editModalElement.querySelector(".card-order").value = cardOrder;
-    editModalElement.querySelector(".card-user").value = cardUser;
-    editModalElement.querySelector(".list-id").value = listId;
-    editModalElement.querySelector(".card-color").value = currentColor;
+    editModalElement.classList.add('is-active');
+    editModalElement.querySelector('.card-id').value = cardId;
+    editModalElement.querySelector('.card-order').value = cardOrder;
+    editModalElement.querySelector('.card-user').value = cardUser;
+    editModalElement.querySelector('.list-id').value = listId;
+    editModalElement.querySelector('.card-color').value = currentColor;
   },
 
   //*HIDE EDIT CARD MODAL
   hideEditModalCard() {
-    const modalElement = document.getElementById("editCardModal");
-    modalElement.classList.remove("is-active");
+    const modalElement = document.getElementById('editCardModal');
+    modalElement.classList.remove('is-active');
   },
 
   //*HANDLE EDIT CARD FORM MODAL
   async handleEditCardForm(event) {
     event.preventDefault();
 
-    const cardId = event.target.querySelector(".card-id").value;
+    const cardId = event.target.querySelector('.card-id').value;
     const targetCardElement = document.querySelector(`[data-card-id='${cardId}']`);
     //info current card
-    const currentTitleCard = targetCardElement.querySelector(".card-info").textContent;
-    const currentDescriptionCard = targetCardElement.querySelector(".card-description").textContent;
+    const currentTitleCard = targetCardElement.querySelector('.card-info').textContent;
+    const currentDescriptionCard = targetCardElement.querySelector('.card-description').textContent;
     //convert RGB to Hex
     let currentColorCard = targetCardElement.style.borderTopColor;
-    let valueRGB = currentColorCard.split("(")[1].split(")")[0];
+    let valueRGB = currentColorCard.split('(')[1].split(')')[0];
     currentColorCard = converter.getHexFromRGB(valueRGB);
 
     //get info from edit card form
     const data = new FormData(event.target);
 
     //!condition if modify only one value
-    const cardOrder = data.get("card_order");
-    let cardEdit = data.get("card_edit");
-    let cardDescription = data.get("card_description");
-    let cardColor = data.get("card_color");
+    const cardOrder = data.get('card_order');
+    let cardEdit = data.get('card_edit');
+    let cardDescription = data.get('card_description');
+    let cardColor = data.get('card_color');
 
-    cardEdit === "" ? (cardEdit = currentTitleCard) : cardEdit;
-    cardDescription === "" ? (cardDescription = currentDescriptionCard) : cardDescription;
-    cardColor === "" ? (cardColor = currentColorCard) : cardColor;
+    cardEdit === '' ? (cardEdit = currentTitleCard) : cardEdit;
+    cardDescription === '' ? (cardDescription = currentDescriptionCard) : cardDescription;
+    cardColor === '' ? (cardColor = currentColorCard) : cardColor;
 
     const options = {
-      method: "PATCH",
+      method: 'PATCH',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         title: cardEdit,
@@ -281,13 +296,13 @@ const cardModule = {
     if (response.ok) {
       await response.json();
 
-      targetCardElement.querySelector(".card-info").textContent = cardEdit;
-      targetCardElement.querySelector(".card-description").textContent = cardDescription;
+      targetCardElement.querySelector('.card-info').textContent = cardEdit;
+      targetCardElement.querySelector('.card-description').textContent = cardDescription;
       targetCardElement.style.borderTop = `4px solid ${cardColor}`;
 
       cardModule.hideEditModalCard();
     } else {
-      throw new Error("Cannot edit this card, server error");
+      throw new Error('Cannot edit this card, server error');
     }
   }
 };
--- assets/js/list.js
+++ assets/js/list.js
@@ -1,6 +1,6 @@
-import { cardModule } from "./index.js";
-import { url, allLists } from "./index.js";
-import { dragList, displayNotification } from "./index.js";
+import { cardModule } from './index.js';
+import { url, allLists } from './index.js';
+import { dragList, displayNotification } from './index.js';
 
 const listModule = {
   //*FETCH ALL LISTS
@@ -15,10 +15,10 @@ const listModule = {
         listModule.makeListInDOM(list.id, list.title, list.description, list.user_id, list.order);
       }
 
-      const buttonsAddCard = document.querySelectorAll(".addCardButton");
+      const buttonsAddCard = document.querySelectorAll('.addCardButton');
 
       for (const button of buttonsAddCard) {
-        button.addEventListener("click", cardModule.showAddCardModal);
+        button.addEventListener('click', cardModule.showAddCardModal);
       }
       //button remove list
       listModule.buttonRemoveList();
@@ -30,18 +30,18 @@ const listModule = {
   //*SHOW LIST MODAL
   showAddListModal() {
     //reset all inputs
-    document.querySelector(`#addListModal input[name='list_name']`).value = "";
-    document.querySelector(`#addListModal input[name='list_description']`).value = "";
+    document.querySelector(`#addListModal input[name='list_name']`).value = '';
+    document.querySelector(`#addListModal input[name='list_description']`).value = '';
 
     //display list form to create a list
-    const modalElement = document.getElementById("addListModal");
-    modalElement.classList.add("is-active");
+    const modalElement = document.getElementById('addListModal');
+    modalElement.classList.add('is-active');
   },
 
   //*HIDE MODALS
   hideModals() {
-    const modalElement = document.getElementById("addListModal");
-    modalElement.classList.remove("is-active");
+    const modalElement = document.getElementById('addListModal');
+    modalElement.classList.remove('is-active');
   },
 
   //*HANDLE LIST FORM
@@ -49,14 +49,14 @@ const listModule = {
     event.preventDefault();
     //get all data from list form
     const data = new FormData(event.target);
-    const listName = data.get("list_name");
-    const listDescription = data.get("list_description");
-    const listUser = data.get("list_user");
+    const listName = data.get('list_name');
+    const listDescription = data.get('list_description');
+    const listUser = data.get('list_user');
 
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         title: listName,
@@ -74,11 +74,11 @@ const listModule = {
       //displayNotification(message);
       location.reload();
     } else {
-      throw new Error("Cannot create this list, server error");
+      throw new Error('Cannot create this list, server error');
     }
 
-    const buttonAddCard = document.querySelector(".addCardButton");
-    buttonAddCard.addEventListener("click", cardModule.showAddCardModal);
+    const buttonAddCard = document.querySelector('.addCardButton');
+    buttonAddCard.addEventListener('click', cardModule.showAddCardModal);
 
     listModule.hideModals();
     //~button remove list
@@ -95,22 +95,22 @@ const listModule = {
   //*MAKE NEW LIST
   makeListInDOM(id, title, description, user, order) {
     //~clone our list template
-    const template = document.querySelector("#template-list");
+    const template = document.querySelector('#template-list');
     const clone = document.importNode(template.content, true);
-    const cloneBlockElement = clone.querySelector(".block-to-clone");
+    const cloneBlockElement = clone.querySelector('.block-to-clone');
     //set datas on our list
-    const list = clone.querySelector(".my-list");
-    list.setAttribute("data-list-id", `${id}`);
-    list.setAttribute("data-order-id", `${order}`);
-    list.querySelector(".edit-list-form").addEventListener("submit", listModule.handleAddNewListTitle);
+    const list = clone.querySelector('.my-list');
+    list.setAttribute('data-list-id', `${id}`);
+    list.setAttribute('data-order-id', `${order}`);
+    list.querySelector('.edit-list-form').addEventListener('submit', listModule.handleAddNewListTitle);
 
     //~append to list board
-    const boardLists = document.querySelector(".board-lists");
-    boardLists.insertAdjacentElement("afterbegin", cloneBlockElement);
-    boardLists.querySelector(".list-title").textContent = title;
-    boardLists.querySelector(".list-description").textContent = description;
-    boardLists.querySelector(".list-user").setAttribute("value", `${user}`);
-    boardLists.querySelector(".list-order").setAttribute("value", `${order}`);
+    const boardLists = document.querySelector('.board-lists');
+    boardLists.insertAdjacentElement('afterbegin', cloneBlockElement);
+    boardLists.querySelector('.list-title').textContent = title;
+    boardLists.querySelector('.list-description').textContent = description;
+    boardLists.querySelector('.list-user').setAttribute('value', `${user}`);
+    boardLists.querySelector('.list-order').setAttribute('value', `${order}`);
 
     //~hide form
     listModule.hideModals();
@@ -124,23 +124,23 @@ const listModule = {
 
   //*BUTTON REMOVE LIST
   buttonRemoveList() {
-    const buttonsRemoveList = document.querySelectorAll(".deleteListButton");
+    const buttonsRemoveList = document.querySelectorAll('.deleteListButton');
 
     for (const buttonRemove of buttonsRemoveList) {
-      buttonRemove.addEventListener("click", listModule.confirmModalDeleteList);
+      buttonRemove.addEventListener('click', listModule.confirmModalDeleteList);
     }
   },
 
   //*BUTTON SHOW REMOVE LIST MODAL
   confirmModalDeleteList(event) {
     //closest can select the closest parent that ave the selected class
-    const listToRemove = event.target.closest(".my-list");
+    const listToRemove = event.target.closest('.my-list');
     const listId = listToRemove.dataset.listId;
 
-    const confirmDeleteBtnElement = document.querySelector("#removeModal");
-    confirmDeleteBtnElement.classList.add("is-active");
-    confirmDeleteBtnElement.querySelector(".list-id").value = listId;
-    confirmDeleteBtnElement.querySelector(".form-delete").addEventListener("submit", listModule.buttonConfirmDeleteList);
+    const confirmDeleteBtnElement = document.querySelector('#removeModal');
+    confirmDeleteBtnElement.classList.add('is-active');
+    confirmDeleteBtnElement.querySelector('.list-id').value = listId;
+    confirmDeleteBtnElement.querySelector('.form-delete').addEventListener('submit', listModule.buttonConfirmDeleteList);
 
     listModule.closeModalDeleteBtn();
   },
@@ -149,12 +149,12 @@ const listModule = {
   async buttonConfirmDeleteList(event) {
     event.preventDefault();
 
-    const listIdToRemove = document.querySelector("#removeModal").querySelector(".list-id").value;
+    const listIdToRemove = document.querySelector('#removeModal').querySelector('.list-id').value;
 
     const listToRemove = document.querySelector(`[data-list-id='${listIdToRemove}']`);
 
     const options = {
-      method: "DELETE"
+      method: 'DELETE'
     };
 
     const response = await fetch(`${url}${allLists}/${listIdToRemove}`, options);
@@ -163,80 +163,81 @@ const listModule = {
       const message = await response.json();
       displayNotification(message);
       listModule.hideModalDeleteList();
+      cardModule.doRemoveCardByListId(listIdToRemove);
       //trick to see deletion immediately
       listToRemove.remove();
     } else {
-      throw new Error("Cannot delete this list, server error");
+      throw new Error('Cannot delete this list, server error');
     }
   },
 
   //*BUTTON CLOSE MODAL REMOVE LIST
   closeModalDeleteBtn() {
-    const confirmDeleteBtnElement = document.querySelector("#removeModal");
-    const closeButtons = confirmDeleteBtnElement.querySelectorAll(".close");
+    const confirmDeleteBtnElement = document.querySelector('#removeModal');
+    const closeButtons = confirmDeleteBtnElement.querySelectorAll('.close');
 
     for (const button of closeButtons) {
-      button.addEventListener("click", listModule.hideModalDeleteList);
+      button.addEventListener('click', listModule.hideModalDeleteList);
     }
   },
 
   //*HIDE MODAL REMOVE LIST
   hideModalDeleteList() {
-    const confirmDeleteBtnElement = document.querySelector("#removeModal");
-    confirmDeleteBtnElement.classList.remove("is-active");
+    const confirmDeleteBtnElement = document.querySelector('#removeModal');
+    confirmDeleteBtnElement.classList.remove('is-active');
   },
 
   //*EDIT FORM LIST
   editListForm() {
-    const listsTitleElement = document.querySelectorAll(".list-title");
+    const listsTitleElement = document.querySelectorAll('.list-title');
 
     for (const listsTitle of listsTitleElement) {
-      listsTitle.addEventListener("click", listModule.displayEditListForm);
+      listsTitle.addEventListener('click', listModule.displayEditListForm);
     }
   },
 
   //*DISPLAY EDIT FORM LIST
   displayEditListForm(event) {
     const targetList = event.target.closest(`[data-list-id]`);
-    targetList.querySelector(".list-description").classList.toggle("display-none");
-    const listButtons = targetList.querySelectorAll(".list-btn");
+    targetList.querySelector('.list-description').classList.toggle('display-none');
+    const listButtons = targetList.querySelectorAll('.list-btn');
 
     for (const listButton of listButtons) {
-      listButton.classList.toggle("display-none");
+      listButton.classList.toggle('display-none');
     }
 
-    const editListFormElement = targetList.querySelector(".edit-list-form");
-    editListFormElement.classList.toggle("is-hidden");
-    editListFormElement.querySelector(".new-list").value = "";
-    editListFormElement.querySelector(".new-description").value = "";
+    const editListFormElement = targetList.querySelector('.edit-list-form');
+    editListFormElement.classList.toggle('is-hidden');
+    editListFormElement.querySelector('.new-list').value = '';
+    editListFormElement.querySelector('.new-description').value = '';
   },
 
   //*HANDLE EDIT FORM LIST
   async handleAddNewListTitle(event) {
     event.preventDefault();
     //~get info from current list
-    const listTitleElement = event.target.closest(".my-list");
+    const listTitleElement = event.target.closest('.my-list');
     const listId = listTitleElement.dataset.listId;
-    let currentTitle = listTitleElement.querySelector(".list-title").textContent;
-    let currentDescription = listTitleElement.querySelector(".list-description").textContent;
+    let currentTitle = listTitleElement.querySelector('.list-title').textContent;
+    let currentDescription = listTitleElement.querySelector('.list-description').textContent;
 
     //~get info from form list
     const data = new FormData(event.target);
 
     //if list name or description are not entered, take the current info
-    let listName = data.get("list_name");
-    listName === "" ? (listName = currentTitle) : listName;
+    let listName = data.get('list_name');
+    listName === '' ? (listName = currentTitle) : listName;
 
-    let listDescription = data.get("list_description");
-    listDescription === "" ? (listDescription = currentDescription) : listDescription;
+    let listDescription = data.get('list_description');
+    listDescription === '' ? (listDescription = currentDescription) : listDescription;
 
-    const listOrder = data.get("list_order");
-    const listUser = data.get("list_user");
+    const listOrder = data.get('list_order');
+    const listUser = data.get('list_user');
 
     const options = {
-      method: "PATCH",
+      method: 'PATCH',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         title: listName,
@@ -254,7 +255,7 @@ const listModule = {
       //displayNotification(message);
       location.reload();
     } else {
-      throw new Error("Cannot edit this list, server error");
+      throw new Error('Cannot edit this list, server error');
     }
   }
 };
